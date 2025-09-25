d209fe1f9d808e1814ffe1e9a53d2fbb3e6611d0
Federico Barrionuevo <federicobarrionuevo47@gmail.com>
2025-09-24 21:20:57 -0300

    Add date to events to improve tracking in logs

--- core/src/main/java/BlockDynasty/Economy/domain/events/Event.java
+++ core/src/main/java/BlockDynasty/Economy/domain/events/Event.java
@@ -1,7 +1,18 @@
 package BlockDynasty.Economy.domain.events;
 
+import java.time.Instant;
+
 public abstract class Event implements CancelableEvent {
     private boolean cancelled = false;
+    private final String date;
+
+    public Event(){
+        this.date = Instant.now().toString();
+    }
+
+    public String getDate() {
+        return date;
+    }
 
     public void setCancelled(boolean cancelled) {
         this.cancelled = cancelled;
