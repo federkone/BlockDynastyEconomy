9fc8779b8d2f2172f8c1077a353faddff3de4152
Federico Barrionuevo <federicobarrionuevo47@gmail.com>
2025-09-30 02:48:36 -0300

    license settings

--- Spigot/src/main/java/BlockDynasty/BukkitImplementation/Integrations/vaultUnloked/VaultUnlockedHook.java
+++ Spigot/src/main/java/BlockDynasty/BukkitImplementation/Integrations/vaultUnloked/VaultUnlockedHook.java
@@ -1,18 +1,22 @@
+/**
+ * Copyright 2025 Federico Barrionuevo "@federkone"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package BlockDynasty.BukkitImplementation.Integrations.vaultUnloked;
 
-import BlockDynasty.Economy.aplication.useCase.AccountsUseCase;
-import BlockDynasty.Economy.aplication.useCase.CurrencyUseCase;
-import BlockDynasty.Economy.aplication.useCase.TransactionsUseCase;
-import BlockDynasty.Economy.aplication.useCase.account.CreateAccountUseCase;
-import BlockDynasty.Economy.aplication.useCase.account.SearchAccountUseCase;
-import BlockDynasty.Economy.aplication.useCase.balance.GetBalanceUseCase;
-import BlockDynasty.Economy.aplication.useCase.currency.SearchCurrencyUseCase;
-import BlockDynasty.Economy.aplication.useCase.transaction.DepositUseCase;
-import BlockDynasty.Economy.aplication.useCase.transaction.WithdrawUseCase;
-import BlockDynasty.Economy.domain.entities.account.Account;
-import BlockDynasty.Economy.domain.entities.balance.Money;
-import BlockDynasty.Economy.domain.entities.currency.Currency;
-import BlockDynasty.Economy.domain.result.Result;
+
 import api.IApi;
 import net.milkbowl.vault2.economy.AccountPermission;
 import net.milkbowl.vault2.economy.Economy;
