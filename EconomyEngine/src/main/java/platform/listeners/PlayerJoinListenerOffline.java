89f51fdd69e3c95f7e3bc81f5425d67972d5fe15
Federico Barrionuevo <federicobarrionuevo47@gmail.com>
2025-12-28 19:17:06 -0300

    Improve Join Listener players. Remove function kick player for accounts loading failed

--- EconomyEngine/src/main/java/platform/listeners/PlayerJoinListenerOffline.java
+++ EconomyEngine/src/main/java/platform/listeners/PlayerJoinListenerOffline.java
@@ -3,9 +3,13 @@ package platform.listeners;
 import BlockDynasty.Economy.aplication.useCase.account.CreateAccountUseCase;
 import BlockDynasty.Economy.aplication.useCase.account.getAccountUseCase.GetAccountByNameUseCase;
 import BlockDynasty.Economy.domain.entities.account.Account;
+import BlockDynasty.Economy.domain.result.ErrorCode;
 import BlockDynasty.Economy.domain.result.Result;
 import BlockDynasty.Economy.domain.services.IAccountService;
 import lib.commands.abstractions.IEntityCommands;
+import lib.util.colors.ChatColor;
+import lib.util.colors.Colors;
+import utils.Console;
 
 public class PlayerJoinListenerOffline implements LoadAccount{
     private final GetAccountByNameUseCase getAccountByNameUseCase;
@@ -22,20 +26,24 @@ public class PlayerJoinListenerOffline implements LoadAccount{
 
     @Override
     public void loadAccount(IEntityCommands player) {
-        Result<Account> result = getAccountByNameUseCase.execute(player.getName());
+        Result<Account> result = getAccountByNameUseCase.execute(player.getName()); //esto falla y procede automaticamente a crear cuenta
         if (result.isSuccess()) {
             Result<Void> resultChangeUuid = accountService.checkUuidChange(result.getValue(), player.getUniqueId());
             if(!resultChangeUuid.isSuccess()){
-                player.kickPlayer("Error loading your finance account. Please log in again or contact an administrator.");
+                Console.logError(resultChangeUuid.getErrorMessage());
+                player.sendMessage(ChatColor.stringValueOf(Colors.RED)+"Error loading your finance account. Please log in again or contact an administrator.");
                 return;
             }
             accountService.addAccountToOnline(result.getValue());
-            return;
-        }
-
-        Result<Account> creationResult = createAccountUseCase.execute(player.getUniqueId(), player.getName());
-        if (!creationResult.isSuccess()) {
-            player.kickPlayer("Error creating or loading your finance account. Please log in again or contact an administrator.");
+        }else if(result.getErrorCode() == ErrorCode.ACCOUNT_NOT_FOUND){
+                Result<Account> creationResult = createAccountUseCase.execute(player.getUniqueId(), player.getName());
+                if (!creationResult.isSuccess()) {
+                    Console.logError(creationResult.getErrorMessage());
+                    player.sendMessage(ChatColor.stringValueOf(Colors.RED)+"Error creating your finance account. Please log in again or contact an administrator.");
+                }
+        }else{
+            Console.logError(result.getErrorMessage());
+            player.sendMessage(ChatColor.stringValueOf(Colors.RED)+"Error loading your finance account. Please log in again or contact an administrator.");
         }
     }
 }
--- EconomyEngine/src/main/java/platform/listeners/PlayerJoinListenerOnline.java
+++ EconomyEngine/src/main/java/platform/listeners/PlayerJoinListenerOnline.java
@@ -3,9 +3,13 @@ package platform.listeners;
 import BlockDynasty.Economy.aplication.useCase.account.CreateAccountUseCase;
 import BlockDynasty.Economy.aplication.useCase.account.getAccountUseCase.GetAccountByUUIDUseCase;
 import BlockDynasty.Economy.domain.entities.account.Account;
+import BlockDynasty.Economy.domain.result.ErrorCode;
 import BlockDynasty.Economy.domain.result.Result;
 import BlockDynasty.Economy.domain.services.IAccountService;
 import lib.commands.abstractions.IEntityCommands;
+import lib.util.colors.ChatColor;
+import lib.util.colors.Colors;
+import utils.Console;
 
 public class PlayerJoinListenerOnline implements LoadAccount{
     private final GetAccountByUUIDUseCase getAccountByUUIDUseCase;
@@ -21,20 +25,24 @@ public class PlayerJoinListenerOnline implements LoadAccount{
     }
 
     public void loadAccount(IEntityCommands player) {
-        Result<Account> result = getAccountByUUIDUseCase.execute(player.getUniqueId());
+        Result<Account> result = getAccountByUUIDUseCase.execute(player.getUniqueId());//esto falla y procede automaticamente a crear cuenta
         if (result.isSuccess()) {
             Result<Void> resultChangeName = accountService.checkNameChange(result.getValue(), player.getName());
             if(!resultChangeName.isSuccess()){
-                player.kickPlayer("Error loading your finance account. Please log in again or contact an administrator.");
+                Console.logError(resultChangeName.getErrorMessage());
+                player.sendMessage(ChatColor.stringValueOf(Colors.RED)+"Error loading your finance account. Please log in again or contact an administrator.");
                 return;
             }
             accountService.addAccountToOnline(result.getValue());
-            return;
-        }
-
-        Result<Account> creationResult = createAccountUseCase.execute(player.getUniqueId(), player.getName());
-        if (!creationResult.isSuccess()) {
-            player.kickPlayer("Error creating or loading your finance account. Please log in again or contact an administrator.");
+        }else if(result.getErrorCode() == ErrorCode.ACCOUNT_NOT_FOUND){
+            Result<Account> creationResult = createAccountUseCase.execute(player.getUniqueId(), player.getName());
+            if (!creationResult.isSuccess()) {
+                Console.logError(creationResult.getErrorMessage());
+                player.sendMessage(ChatColor.stringValueOf(Colors.RED)+"Error creating your finance account. Please log in again or contact an administrator.");
+            }
+        }else{
+            Console.logError(result.getErrorMessage());
+            player.sendMessage(ChatColor.stringValueOf(Colors.RED)+"Error loading your finance account. Please log in again or contact an administrator.");
         }
     }
 }
