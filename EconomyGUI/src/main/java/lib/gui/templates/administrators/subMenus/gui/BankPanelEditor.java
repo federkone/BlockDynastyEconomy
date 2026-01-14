f60388ad51642f048026333d34e4c881190e4e3b
Federico Barrionuevo <federicobarrionuevo47@gmail.com>
2026-01-14 16:12:11 -0300

    add licences

--- EconomyGUI/src/main/java/lib/gui/templates/administrators/subMenus/gui/BankPanelEditor.java
+++ EconomyGUI/src/main/java/lib/gui/templates/administrators/subMenus/gui/BankPanelEditor.java
@@ -1,6 +1,23 @@
+/**
+ * Copyright 2025 Federico Barrionuevo "@federkone"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package lib.gui.templates.administrators.subMenus.gui;
 
 import lib.abstractions.IConfigurationGUI;
+import lib.gui.GUIFactory;
 import lib.gui.components.IEntityGUI;
 import lib.gui.components.IGUI;
 import lib.gui.components.factory.Item;
@@ -166,6 +183,6 @@ public class BankPanelEditor extends AbstractPanel {
 
     @Override
     public void refresh() {
-        setupGUI();
+        GUIFactory.bankPanelEditor(player,getParent()).open();
     }
 }
