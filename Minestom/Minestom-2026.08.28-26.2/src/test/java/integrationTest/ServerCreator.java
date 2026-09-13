ed306c7732f53ddaf1cd90b87e2fa79af6d97a10
Federico Barrionuevo <federicobarrionuevo47@gmail.com>
2026-09-13 19:54:01 -0300

    minestom 26.2

---
+++ Minestom/Minestom-2026.08.28-26.2/src/test/java/integrationTest/ServerCreator.java
@@ -0,0 +1,30 @@
+package integrationTest;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.LightingChunk;
+import net.minestom.server.instance.block.Block;
+
+public class ServerCreator {
+
+    public static void start(){
+        MinecraftServer minecraftServer = MinecraftServer.init();
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+        instanceContainer.setTime(6000);
+        instanceContainer.setChunkSupplier(LightingChunk::new);
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+        minecraftServer.start("0.0.0.0", 25565);
+    }
+}
---
+++ Minestom/Minestom-2026.08.28-26.2/src/test/java/integrationTest/basicCommands/toggleOp.java
@@ -0,0 +1,16 @@
+package integrationTest.basicCommands;
+
+import net.blockdynasty.economy.minestom.commons.services.PermsServiceDefault;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.entity.Player;
+
+public class toggleOp extends Command {
+
+    public toggleOp() {
+        super("op");
+        setDefaultExecutor((sender, commandContext) -> {
+            Player player = (Player) sender;
+            PermsServiceDefault.setOp(player, !PermsServiceDefault.isOp(player));
+        });
+    }
+}
